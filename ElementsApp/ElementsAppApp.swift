5ac1be33df6d6a45b99fb2b5687dfb9703ce2526
Albina <olifirenko.albina@gmail.com>
2022-10-23 18:23:20 +0300

    Initial Commit

---
+++ ElementsApp/ElementsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementsAppApp.swift
+//  ElementsApp
+//
+//  Created by mac on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ElementsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
